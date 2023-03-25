d228c2207179b0856a094f30ee7d6c42dfb96f6c
Admin <Admin@10.9.13.177>
2023-03-25 08:04:54 -0500

    Inicializacion del proyecto

--- src/sistemaventarestaurante/SistemaVentaRestaurante.java
+++ src/sistemaventarestaurante/SistemaVentaRestaurante.java
@@ -2,9 +2,8 @@
 package sistemaventarestaurante;
 
 public class SistemaVentaRestaurante {
-
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
